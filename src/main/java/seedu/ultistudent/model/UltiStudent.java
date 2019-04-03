10d4bbef844edcd4e3aaef098a22e847d6d34ebe
NgWeiXin <xavier_ng@icloud.com>
2019-04-03 18:44:33 +0800

    Removed Persons, modified equals() and fixed typos in Javadocs

--- src/main/java/seedu/ultistudent/model/UltiStudent.java
+++ src/main/java/seedu/ultistudent/model/UltiStudent.java
@@ -15,17 +15,13 @@ import seedu.ultistudent.model.homework.Homework;
 import seedu.ultistudent.model.homework.UniqueHomeworkList;
 import seedu.ultistudent.model.note.Note;
 import seedu.ultistudent.model.note.UniqueNoteList;
-import seedu.ultistudent.model.person.Person;
-import seedu.ultistudent.model.person.UniquePersonList;
 
 /**
- * Wraps all data at the ultistudent-book level
- * Duplicates are not allowed (by .isSamePerson comparison)
+ * Wraps all data at the ultistudent level
  */
 public class UltiStudent implements ReadOnlyUltiStudent {
 
     private final UniqueCapEntryList capEntryList;
-    private final UniquePersonList persons;
     private final UniqueHomeworkList homeworkList;
     private final UniqueModuleSemesterList moduleSemesterList;
     private final UniqueNoteList noteList;
@@ -39,7 +35,6 @@ public class UltiStudent implements ReadOnlyUltiStudent {
      *   among constructors.
      */
     {
-        persons = new UniquePersonList();
         capEntryList = new UniqueCapEntryList();
         homeworkList = new UniqueHomeworkList();
         noteList = new UniqueNoteList();
@@ -49,27 +44,19 @@ public class UltiStudent implements ReadOnlyUltiStudent {
     public UltiStudent() {}
 
     /**
-     * Creates an UltiStudent using the Persons in the {@code toBeCopied}
+     * Creates an UltiStudent using capEntryList, homeworkList, noteList and moduleSemesterList
+     * in the {@code toBeCopied}
      */
     public UltiStudent(ReadOnlyUltiStudent toBeCopied) {
         this();
         resetData(toBeCopied);
     }
 
-    //// list overwrite operations
-
-    /**
-     * Replaces the contents of the person list with {@code persons}.
-     * {@code persons} must not contain duplicate persons.
-     */
-    public void setPersons(List<Person> persons) {
-        this.persons.setPersons(persons);
-        indicateModified();
-    }
+    //===== list overwrite operations =====//
 
     /**
      * Replaces the contents of the cap entries list with {@code capEntryList}.
-     * {@code capEntryList} must not contain duplicate persons.
+     * {@code capEntryList} must not contain duplicate cap entry.
      */
     public void setCapEntryList(List<CapEntry> capEntryList) {
         this.capEntryList.setCapEntryList(capEntryList);
@@ -77,8 +64,8 @@ public class UltiStudent implements ReadOnlyUltiStudent {
     }
 
     /**
-     * Replaces the contents of the cap entries list with {@code capEntryList}.
-     * {@code capEntryList} must not contain duplicate persons.
+     * Replaces the contents of the ModuleSemesterlist with {@code moduleSemesterList}.
+     * {@code moduleSemesterList} must not contain duplicate ModuleSemester.
      */
     public void setModuleSemesterList(List<ModuleSemester> moduleSemesterList) {
         this.moduleSemesterList.setModuleSemesterList(moduleSemesterList);
@@ -91,45 +78,18 @@ public class UltiStudent implements ReadOnlyUltiStudent {
     public void resetData(ReadOnlyUltiStudent newData) {
         requireNonNull(newData);
         setCapEntryList(newData.getCapEntryList());
-        setPersons(newData.getPersonList());
         setHomework(newData.getHomeworkList());
         setModuleSemesterList(newData.getModuleSemesterList());
         setNote(newData.getNoteList());
     }
 
-    //// person-level operations
-
+    //===== CapManager's operations =====//
     /**
-     * Returns true if a person with the same identity as {@code person} exists in the UltiStudent.
-     */
-    public boolean hasPerson(Person person) {
-        requireNonNull(person);
-        return persons.contains(person);
-    }
-
-    /**
-     * Returns true if a cap entry with the same identity as {@code person} exists in UltiStudent.
+     * Returns true if a cap entry with the same identity as {@code capEntry} exists in UltiStudent.
      */
     public boolean hasCapEntry(CapEntry capEntry) {
         requireNonNull(capEntry);
         return capEntryList.contains(capEntry);
-    }
-
-    /**
-     * Returns true if a module semester with the same identity as {@code moduleSemester} exists in UltiStudent.
-     */
-    public boolean hasModuleSemester(ModuleSemester moduleSemester) {
-        requireNonNull(moduleSemester);
-        return moduleSemesterList.contains(moduleSemester);
-    }
-
-    /**
-     * Adds a person to the UltiStudent.
-     * The person must not already exist in the UltiStudent.
-     */
-    public void addPerson(Person p) {
-        persons.add(p);
-        indicateModified();
     }
 
     /**
@@ -142,58 +102,15 @@ public class UltiStudent implements ReadOnlyUltiStudent {
     }
 
     /**
-     * Adds a cap entry to UltiStudent.
-     * The cap entry must not already exist in UltiStudent.
-     */
-    public void addModuleSemester(ModuleSemester moduleSemester) {
-        moduleSemesterList.add(moduleSemester);
-        indicateModified();
-    }
-
-    /**
-     * Replaces the given person {@code target} in the list with {@code editedPerson}.
-     * {@code target} must exist in the UltiStudent.
-     * The person identity of {@code editedPerson} must not be the same as another existing person in the UltiStudent.
-     */
-    public void setPerson(Person target, Person editedPerson) {
-        requireNonNull(editedPerson);
-
-        persons.setPerson(target, editedPerson);
-        indicateModified();
-    }
-
-    /**
      * Replaces the given cap entry {@code target} in the list with {@code editedCapEntry}.
      * {@code target} must exist in the UltiStudent.
-     * The cap entry identity of {@code editedPerson} must not be the same as another existing cap entry in
+     * The cap entry identity of {@code editedCapEntry} must not be the same as another existing cap entry in
      * the UltiStudent.
      */
     public void setCapEntry(CapEntry target, CapEntry editedCapEntry) {
         requireNonNull(editedCapEntry);
 
         capEntryList.setCapEntry(target, editedCapEntry);
-        indicateModified();
-    }
-
-    /**
-     * Replaces the given cap entry {@code target} in the list with {@code editedCapEntry}.
-     * {@code target} must exist in the UltiStudent.
-     * The cap entry identity of {@code editedPerson} must not be the same as another existing cap entry in
-     * the UltiStudent.
-     */
-    public void setModuleSemester(ModuleSemester target, ModuleSemester editedModuleSemester) {
-        requireNonNull(editedModuleSemester);
-
-        moduleSemesterList.setModuleSemester(target, editedModuleSemester);
-        indicateModified();
-    }
-
-    /**
-     * Removes {@code key} from this {@code UltiStudent}.
-     * {@code key} must exist in the UltiStudent.
-     */
-    public void removePerson(Person key) {
-        persons.remove(key);
         indicateModified();
     }
 
@@ -207,6 +124,36 @@ public class UltiStudent implements ReadOnlyUltiStudent {
     }
 
     /**
+     * Adds a moduleSemester to UltiStudent.
+     * The moduleSemester must not already exist in UltiStudent.
+     */
+    public void addModuleSemester(ModuleSemester moduleSemester) {
+        moduleSemesterList.add(moduleSemester);
+        indicateModified();
+    }
+
+    /**
+     * Returns true if a module semester with the same identity as {@code moduleSemester} exists in UltiStudent.
+     */
+    public boolean hasModuleSemester(ModuleSemester moduleSemester) {
+        requireNonNull(moduleSemester);
+        return moduleSemesterList.contains(moduleSemester);
+    }
+
+    /**
+     * Replaces the given ModuleSemester {@code target} in the list with {@code editedModuleSemester}.
+     * {@code target} must exist in the UltiStudent.
+     * The ModuleSemester identity of {@code editedModuleSemester} must not be the same as another
+     * existing ModuleSemester in the UltiStudent.
+     */
+    public void setModuleSemester(ModuleSemester target, ModuleSemester editedModuleSemester) {
+        requireNonNull(editedModuleSemester);
+
+        moduleSemesterList.setModuleSemester(target, editedModuleSemester);
+        indicateModified();
+    }
+
+    /**
      * Removes {@code key} from this {@code UltiStudent}.
      * {@code key} must exist in the UltiStudent.
      */
@@ -215,6 +162,7 @@ public class UltiStudent implements ReadOnlyUltiStudent {
         indicateModified();
     }
 
+    //===== HomeworkManager's operations =====//
     public void setHomework(Homework target, Homework editedHomework) {
         requireNonNull(editedHomework);
 
@@ -228,7 +176,7 @@ public class UltiStudent implements ReadOnlyUltiStudent {
     }
 
     /**
-     * Returns true if a person with the same identity as {@code person} exists in the UltiStudent.
+     * Returns true if a homework with the same identity as {@code homework} exists in the UltiStudent.
      */
     public boolean hasHomework(Homework homework) {
         requireNonNull(homework);
@@ -236,8 +184,8 @@ public class UltiStudent implements ReadOnlyUltiStudent {
     }
 
     /**
-     * Adds a person to the UltiStudent.
-     * The person must not already exist in the UltiStudent.
+     * Adds a homework to the UltiStudent.
+     * The homework must not already exist in the UltiStudent.
      */
     public void addHomework(Homework homework) {
         this.homeworkList.add(homework);
@@ -253,7 +201,7 @@ public class UltiStudent implements ReadOnlyUltiStudent {
         indicateModified();
     }
 
-    //===== Note Manager =====
+    //===== NotesManager's operations =====//
     public void setNote(Note target, Note editedNote) {
         requireNonNull(editedNote);
 
@@ -311,18 +259,7 @@ public class UltiStudent implements ReadOnlyUltiStudent {
         invalidationListenerManager.callListeners(this);
     }
 
-    //// util methods
-
-    @Override
-    public String toString() {
-        return persons.asUnmodifiableObservableList().size() + " persons";
-        // TODO: refine later
-    }
-
-    @Override
-    public ObservableList<Person> getPersonList() {
-        return persons.asUnmodifiableObservableList();
-    }
+    //===== util methods =====//
 
     @Override
     public ObservableList<CapEntry> getCapEntryList() {
@@ -348,11 +285,9 @@ public class UltiStudent implements ReadOnlyUltiStudent {
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof UltiStudent // instanceof handles nulls
-                && persons.equals(((UltiStudent) other).persons));
-    }
-
-    @Override
-    public int hashCode() {
-        return persons.hashCode();
+                && capEntryList.equals(((UltiStudent) other).capEntryList)
+                && homeworkList.equals(((UltiStudent) other).homeworkList)
+                && noteList.equals(((UltiStudent) other).noteList)
+                && moduleSemesterList.equals(((UltiStudent) other).moduleSemesterList));
     }
 }
